94d4a24cb9c565c21ff96065e548546108aba487
FOG <dn135897@gmail.com>
2024-01-16 09:31:04 +0700

    Update ChuongTrinhChinh.class
    Nghỉ ngơi

--- DemoTenTuoi/src/DuyDan/ChuongTrinhChinh.java
+++ DemoTenTuoi/src/DuyDan/ChuongTrinhChinh.java
@@ -1,10 +1,23 @@
 package DuyDan;
+import java.util.Scanner;
 
 public class ChuongTrinhChinh {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int namSinh;
+		String hoTen;
+		Scanner banphim = new Scanner(System.in);
+		
+		System.out.print("Họ và tên: ");
+		hoTen = banphim.nextLine(); 
+		
+		System.out.print("Năm sinh: ");
+		namSinh = banphim.nextInt(); 
+		
+		int tuoi = 2024 - namSinh + 1;
+		
+		System.out.printf("Tuổi: %d",tuoi);
 	}
 
 }
