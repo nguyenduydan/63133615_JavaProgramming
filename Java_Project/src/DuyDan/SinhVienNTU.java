def913fef61a9c7c10efa3da3088f36e94200238
FOG <dn135897@gmail.com>
2024-01-23 09:32:16 +0700

    Create SinhVienNTU.java

---
+++ Java_Project/src/DuyDan/SinhVienNTU.java
@@ -0,0 +1,35 @@
+package DuyDan;
+
+public class SinhVienNTU {
+	String hoTen;
+	String nganh;
+	
+	public SinhVienNTU(String hoTen, String nganh) {
+		super();
+		this.hoTen = hoTen;
+		this.nganh = nganh;
+	}
+
+	public SinhVienNTU() {
+	}
+	
+	
+
+	public String getHoTen() {
+		return hoTen;
+	}
+
+	public void setHoTen(String hoTen) {
+		this.hoTen = hoTen;
+	}
+
+	public String getNganh() {
+		return nganh;
+	}
+
+	public void setNganh(String nganh) {
+		this.nganh = nganh;
+	}
+
+	
+}
