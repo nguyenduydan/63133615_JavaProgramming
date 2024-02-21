2b58bf746a2ed6727b1953ef20e5c049db755752
FOG <dn135897@gmail.com>
2024-02-21 23:00:43 +0700

    Finished Lab5Bai2

--- Lab5/src/Bai2.java
+++ Lab5/src/Bai2.java
@@ -2,7 +2,34 @@ import java.util.*;
 public class Bai2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList<String>ds = new ArrayList<>();
+		Scanner scanner = new Scanner(System.in);
+		 while (true) {
+	            int choice = Menu(scanner);
+	
+	            switch (choice) {
+	                case 1:
+	                    nhap(ds, scanner);
+	                    break;
+	                case 2:
+	                    xuat(ds);
+	                    break;
+	                case 3:
+	                    ngaunhien(ds);
+	                    break;
+	                case 4:
+	                    sapxep(ds);
+	                    break;
+	                case 5:
+	                	xoa(ds, scanner);
+	                	break;
+	                case 6:
+	                	ketthuc();
+	                	return;
+	                default:
+	                    System.out.println("Lựa chọn không hợp lệ. Vui lòng chọn lại.");
+	            }
+	        }
 
 	}
 	
@@ -19,4 +46,51 @@ public class Bai2 {
         return scanner.nextInt();
 	}
 	
+	private static void nhap(ArrayList<String> list, Scanner scanner) {
+		scanner.nextLine(); 
+        System.out.println("Nhập danh sách họ và tên (nhập 'done' để kết thúc):");
+		while(true) {
+			scanner = new Scanner(System.in);
+			String hoTen = scanner.nextLine();
+			if(hoTen.equalsIgnoreCase("done")) {
+				break;
+			}
+			list.add(hoTen);
+		}
+	}
+	
+	private static void xuat(ArrayList<String> list) {
+		System.out.println("Danh sách họ và tên");
+		for(String ds: list) {
+			System.out.println(ds);
+		}
+	}
+	private static void sapxep(ArrayList<String> list){
+		Collections.sort(list);
+		System.out.println("Danh sách tăng dần:");
+		xuat(list);
+		Collections.reverse(list);
+		System.out.println("Danh sách giảm dần:");
+		xuat(list); 
+	}
+	private static void ngaunhien(ArrayList<String> list) {
+		Collections.shuffle(list);
+		System.out.println("Danh sách ngẫu nhiên:");
+		xuat(list);
+	}
+	private static void xoa(ArrayList<String> list, Scanner scanner) {
+        scanner.nextLine();
+        System.out.print("Nhập họ tên cần xóa: ");
+        String hoTen = scanner.nextLine();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equalsIgnoreCase(hoTen)) {
+                list.remove(i);
+                System.out.println("Đã xóa: " + hoTen);
+                break;
+            }
+        }
+    }
+ 	private static void ketthuc() {
+        System.out.println("Chương trình kết thúc.");
+    }
 }
