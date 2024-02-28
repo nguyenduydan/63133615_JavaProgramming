28cc0abcf8e473233cdea3cb408581a9fd84543a
Duy Đan <dn135897@gmail.com>
2024-02-28 15:51:33 +0700

    Create GameTicTacToe

---
+++ THPlus_TicTacToe/src/CTC.java
@@ -0,0 +1,11 @@
+
+public class CTC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ScreenGame screen =  new ScreenGame();
+		screen.setVisible(true);
+		
+	}
+
+}
